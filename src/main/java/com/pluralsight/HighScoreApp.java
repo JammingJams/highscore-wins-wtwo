f3423f956390c0d8be431743927c480b552999ec
JammingJams <noahlogan20o00@gmail.com>
2026-04-14 08:59:51 -0400

    Initial commit

--- src/main/java/com/pluralsight/HighScoreApp.java
+++ src/main/java/com/pluralsight/HighScoreApp.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class HighScoreApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please put in a score for the game: ");
+        String userGameScore = sc.nextLine();
+
+        String[] tempUser = userGameScore.split( "[:|]");
+
+        int numOne = Integer.parseInt(tempUser[2]);
+        int numTwo = Integer.parseInt(tempUser[3]);
+
+        if (numOne > numTwo) {
+            System.out.printf("Winner: %s",tempUser[0]);
+        }
+        else {
+            System.out.printf("Winner: %s",tempUser[1]);
+        }
+
+    }
 }
